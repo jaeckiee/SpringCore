e2740fb67f691a4f70140eca39f11fa3afeaf25c
jaeckiee <jaechan11@gmail.com>
2023-04-01 20:59:49 +0900

    test : implement sigleton pattern

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -15,4 +15,12 @@ public class SingletonTest {
         MemberService memberService2 = appConfig.memberService();
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
 }
